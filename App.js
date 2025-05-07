5734412f7de5b2c97dec9d281ae951bfc9020de7
lhoiles <laurahoiles639@gmail.com>
2025-05-07 22:33:38 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,65 +1,26 @@
-// Import necessary hooks from react
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-// Import routing tools from react-router-dom
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// Import Axios to make HTTP requests to the backend
-import axios from "axios";
-
-// Import your custom components
-import AddRecipeForm from "./AddRecipeForm";
+// Import your page components
 import RecipeList from "./RecipeList";
-import RecipeDetails from "./RecipeDetails"; // This will be the page for a single recipe view
+import MyRecipes from "./MyRecipes";
 
 function App() {
-  // This state holds all recipes fetched from the backend
-  const [recipes, setRecipes] = useState([]);
+    return (
+        <Router>
+            {/* Top navigation bar */}
+            <nav style={{ padding: "1rem", backgroundColor: "#f2f2f2" }}>
+                <Link to="/" style={{ marginRight: "1rem" }}>Home</Link>
+                <Link to="/my-recipes">My Recipes</Link>
+            </nav>
 
-  // Run fetchRecipes() when the component mounts
-  useEffect(() => {
-    fetchRecipes();
-  }, []);
-
-  // This function fetches all recipes from the backend API
-  const fetchRecipes = () => {
-    axios
-      .get("http://localhost/recipe-api/get_recipes.php") // Call PHP endpoint
-      .then((response) => {
-        setRecipes(response.data); // Save the list of recipes to state
-      })
-      .catch((error) => {
-        console.error("Error fetching recipes:", error);
-      });
-  };
-
-  return (
-    // Router wraps the entire app and enables navigation between pages
-    <Router>
-      <div style={{ padding: "2rem", maxWidth: "700px", margin: "auto" }}>
-        {/* Application heading */}
-        <h1>Recipe Manager</h1>
-
-        {/* Define all routes in the application */}
-        <Routes>
-          {/* Home route: shows the add form and recipe list */}
-          <Route
-            path="/"
-            element={
-              <>
-                <AddRecipeForm onRecipeAdded={fetchRecipes} />
-                <RecipeList recipes={recipes} onUpdate={fetchRecipes} />
-              </>
-            }
-          />
-
-          {/* Dynamic route for viewing an individual recipe by its ID */}
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+            {/* Page routes */}
+            <Routes>
+                <Route path="/" element={<RecipeList />} />
+                <Route path="/my-recipes" element={<MyRecipes />} />
+            </Routes>
+        </Router>
+    );
 }
 
-// Export the App component so it can be rendered in index.js
 export default App;
